0d153a417d362530b8c2ec92c8f4f4da5e3888fa
John Stewart <jstewartstack@gmail.com>
2022-12-07 18:41:46 +0000

    learningvariables

---
+++ Variables/Variables.java
@@ -0,0 +1,9 @@
+package Variables;
+
+public class Variables {
+     public static void main(String[] args) {
+          int passengers = 5;
+          System.out.println(passengers);
+     }     
+}
+ 
